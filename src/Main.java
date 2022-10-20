4026fd096d8340a27ffed88ab5e55931247051b2
Daysuke <daysuke.rojas@gmail.com>
2022-10-20 12:56:46 -0500

    Generator Pass

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+
+    static String generaContrasenia(){
+        char[] mayusculas = {'A', 'B', 'C', 'D', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
+
+        return "";
     }
 }
