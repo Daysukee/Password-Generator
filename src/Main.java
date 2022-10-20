16de4e7f333a9924187c339d66920188cc386dbc
Daysuke <daysuke.rojas@gmail.com>
2022-10-20 13:35:21 -0500

    Generator Pass

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(generaContrasenia());
     }
 
     static String generaContrasenia(){
-        char[] mayusculas = {'A', 'B', 'C', 'D', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
+        char[] mayusculas = {'A', 'B', 'C', 'D', 'E','F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        char[] minusculas = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char[] numeros = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
 
-        return "";
+        StringBuilder caracteres = new StringBuilder();
+        caracteres.append(mayusculas);
+        caracteres.append(minusculas);
+        caracteres.append(numeros);
+
+        StringBuilder contraseña = new StringBuilder();
+
+        // Cantidad de caracteres
+        for (int i = 0; i <= 15; i++){
+            int cantidadCaracteres = caracteres.length();
+            int numeroRandom = (int) (Math.random()*cantidadCaracteres);
+
+            contraseña.append((caracteres.toString()).charAt(numeroRandom));
+        }
+
+
+
+        return contraseña.toString();
     }
 }
